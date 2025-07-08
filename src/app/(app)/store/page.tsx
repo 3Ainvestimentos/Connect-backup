6707df32eae90828be20254d717dd7478b5f6507
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-07-08 18:28:46 +0000

    indexe esse codigo no "store"
    import React from 'react';
    
    const NuvemSh

--- src/app/(app)/store/page.tsx
+++ src/app/(app)/store/page.tsx
@@ -1,24 +1,17 @@
+import React from 'react';
 
-import { PageHeader } from '@/components/layout/PageHeader';
-import { ShoppingCart } from 'lucide-react';
-
-export default function StorePage() {
+const NuvemShopEmbed = () => {
   return (
-    <div className="flex flex-col h-full">
-      <div className="p-6 md:p-8 pb-0">
-        <PageHeader 
-          title="Store" 
-          icon={ShoppingCart}
-          description="Navegue e compre na nossa loja corporativa."
-        />
-      </div>
-      <div className="flex-grow p-6 md:p-8 pt-2">
-        <iframe
-          src="https://www.store-3ariva.com.br/"
-          title="Store 3A RIVA"
-          className="w-full h-full border rounded-lg bg-card"
-        />
-      </div>
+    <div style={{ width: '100%', height: '100vh' }}>
+      <iframe 
+        src="https://www.store-3ariva.com.br/" 
+        width="100%" 
+        height="100%" 
+        frameBorder="0"
+        title="Nuvem Shop Store"
+      />
     </div>
   );
-}
+};
+
+export default NuvemShopEmbed;
