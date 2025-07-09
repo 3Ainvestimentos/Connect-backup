d139de9226f8590eef4e157146debfbe86700213
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-07-09 20:33:38 +0000

    No Botão de Login use a logo do Google

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -46,9 +46,7 @@ export default function LoginPage() {
           {loading ? (
             <Loader2 className="mr-2 h-5 w-5 animate-spin" />
           ) : (
-            <svg className="mr-2 h-5 w-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-              <path fill="currentColor" d="M488 261.8C488 403.3 381.5 512 244 512 110.3 512 0 398.5 0 256S110.3 0 244 0c73 0 135.5 24.3 184.5 63.6L373.8 120c-35.1-33.3-83.3-53.8-129.8-53.8-106 0-191.5 85.5-191.5 191.2s85.5 191.2 191.5 191.2c71.2 0 123-30.8 159.3-64.8 28.2-26.6 42.6-67.7 47.4-113.1H244V261.8h244z"></path>
-            </svg>
+            <Image src="https://i.ibb.co/7jQqMv7/google-logo.png" alt="Google logo" width={20} height={20} className="mr-2" />
           )}
           Entrar com Google
         </Button>
