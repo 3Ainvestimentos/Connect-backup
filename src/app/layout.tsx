df0de39dc3d3f61f75831dc88a3fb658a00bb36d
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-07-15 15:28:04 +0000

    O sistema não está reconhecendo caracteres especiais como "Ç" e vogais a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        <meta charSet="utf-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet" />
