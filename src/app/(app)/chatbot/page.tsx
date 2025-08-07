3aa24b9362a5e8769f7101760712c71c1c0435ef
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-08-07 02:15:17 +0000

    Atualize o link do botão Bob para a versão: https://studio--datavisor-44

--- src/app/(app)/chatbot/page.tsx
+++ src/app/(app)/chatbot/page.tsx
@@ -3,7 +3,7 @@ export default function ChatbotPage() {
   return (
     <div className="h-full w-full">
       <iframe
-        src="https://studio--bob-10-d25zl.us-central1.hosted.app"
+        src="https://studio--datavisor-44i5m.us-central1.hosted.app/chat"
         className="w-full h-full border-none"
         title="Assistente Bob 1.0"
         allow="microphone"
