0756fde7588efa0fba8bae936020cf69df80ba14
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-09-15 19:42:59 +0000

    O ciclo que muda a cor de fundo do FAB voltou a estar muito rápido, fora

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -111,7 +111,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-bg': 'pulse-bg 4s infinite',
+        'pulse-bg': 'pulse-bg 4s ease-in-out infinite',
       },
     },
   },
